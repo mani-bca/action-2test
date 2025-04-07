242ac33d0308bff140d74168771456d337953ab6
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-04-07 05:29:10 +0000

    some change

---
+++ file1.py
@@ -0,0 +1,5 @@
+Diffs provide useful information about your repository in a legible format, making it easier to perform the following actions: 
+
+Review changes while staging. 
+Decide whether to rebase or merge. 
+Copy and paste code from a file. 
---
+++ file2.py
@@ -0,0 +1,7 @@
+From the central graph in GitKraken, hold down the Cmd/Ctrl key to select the head commit of two branches. 
+From here, you will have the option to view the diff between these two branches.
+
+But what happens when you change the order of the branch selection? 
+When you give that a try, you may see deleted files show as “added” or vice versa.
+That’s because direction matters when viewing a Git diff.  
+So keep that in mind as you click the order of branch or commit diffs. 
